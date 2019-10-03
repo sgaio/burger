ec3c46a5e2cf8e08c41386b0302f74f31573e0c5
sgaio <50683085+sgaio@users.noreply.github.com>
2019-10-02 23:55:04 -0700

    App is Working

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Double Cheese Burger",
+0
+),
+(
+"Bacon Burger",
+0
+),
+(
+"Da Bomb",
+0
+);
---
+++ server.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let methodOverride = require("method-override");
+
+let port = 3306;
+
+let app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
